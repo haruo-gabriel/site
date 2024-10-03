7ced2f59f9b58d680192f52e6c652f51c7c4aeb1
adrielias67 <adri.aea.adri@gmail.com>
2024-10-03 15:31:12 -0300

    Janky aah undo functions
    undo functions that save the entire html at every step

--- src/js/save.js
+++ src/js/save.js
@@ -16,3 +16,13 @@ function loadSiteHtml(json) {
     const loadObj = JSON.parse(json);
     mutableDiv.innerHTML = loadObj.content;   
 }
+
+
+//document.getElementById("test").addEventListener("click", addTestDiv);
+//
+//function addTestDiv() {
+//    const testDiv = document.createElement('div');
+//    testDiv.innerText = 'Test div';
+//    document.getElementById("mutable").appendChild(testDiv);
+//    document.getElementById("mutable").appendChild(document.createTextNode('\n'));
+//}
