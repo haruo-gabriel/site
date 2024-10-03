93c83b9462cfdade373ca3271cdfc7866c9c9cd6
haruo <takeuchigabriel@usp.br>
2024-10-03 15:39:29 -0300

    Fixing Maritaca methods

--- src/maritaca/Maritaca.py
+++ src/maritaca/Maritaca.py
@@ -15,28 +15,26 @@ class Maritaca:
   def generate_few_shots_prompt(user_prompt):
     few_shots_prompt = f"""
     Instruções:
-    Gere um elemento <div> com estilos CSS embutidos.
-    Sempre escreva apenas a tag <div>...</div>.
-    Sempre feche a tag <div> completamente com </div>.
-    Retorne apenas o código HTML do elemento <div>...</div> com os estilos CSS embutidos. Não retorne textos a mais.
+    Gere uma resposta, um elemento <div> com estilos CSS embutidos. Certifique-se de escrever apenas a tag <div>...</div> e fechá-la completamente com </div>. Retorne apenas o código HTML do elemento <div>...</div>, sem adicionar texto extra.
 
     Exemplo 1:
-    Descrição: Um botão vermelho com texto branco
-    <div style="background-color: red; color: white; padding: 10px; text-align: center;">Botão</div>
+    Descrição: um botão vermelho com texto branco
+    Resposta: <div style="background-color: red; color: white; padding: 10px;">Botão</div>
 
     Exemplo 2:
-    Descrição: Um parágrafo com texto azul e fonte Arial.
-    <div style="color: blue; font-family: Arial; padding: 10px;">Este é um parágrafo.</div>
+    Descrição: um parágrafo com texto azul e fonte Arial.
+    Resposta: <div style="color: blue; font-family: Arial">Este é um parágrafo.</div>
 
     Exemplo 3:
-    Descrição: Um cabeçalho com fundo amarelo e texto preto
-    <div style="background-color: yellow; color: black; padding: 10px; text-align: center;">Cabeçalho</div>
+    Descrição: um cabeçalho com fundo amarelo e texto preto
+    Resposta: <div style="background-color: yellow; color: black; text-align: center;">Cabeçalho</div>
 
     Exemplo 4:
-    Descrição: Um botão vermelho com texto rosa escrito "merda"
-    <div style="background-color: red; color: pink; padding: 10px; text-align: center;">merda</div>
+    Descrição: um botão vermelho com texto rosa escrito "merda"
+    Resposta: <div style="background-color: red; color: pink; padding: 10px; text-align: center;">merda</div>
 
     Descrição: {user_prompt}
+    Resposta: 
     """
     return few_shots_prompt
 
@@ -45,6 +43,7 @@ class Maritaca:
     response = self.model.generate(
         few_shots_prompt,
         chat_mode=False,
-        max_tokens=300
+        max_tokens=300,
+        temperature=0.5
     )
     return str(response["answer"])
